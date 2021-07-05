c1ea74275524e61b0c37f07be77883e8517f0188
Nate Hogan <natek.hogan@gmail.com>
2021-07-04 22:49:02 -0600

    update portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,8 +9,8 @@ function Portfolio() {
             <h1 data-testid="h1tag">Portfolio</h1>
             <div className="works">
                 
-                <a href="https://nathanhogan16.github.io/run-buddy" id="work" className="hero">
-                    <p className="name">Run-Buddy</p>
+                <a href="https://serene-kings-canyon-83018.herokuapp.com/" id="work" className="hero">
+                    <p className="name">Jacks of all Trades</p>
                 </a>
                 <a href="https://nickrodriquez.github.io/Map-Method/" id="work" className="google">
                     <p className="name">Map-Method</p>
