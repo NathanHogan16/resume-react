6a6be20b1ae16b205e233e31bf660478b91e2fa0
Nate Hogan <natek.hogan@gmail.com>
2021-09-11 17:53:45 -0600

    edits

--- src/components/About.js
+++ src/components/About.js
@@ -9,12 +9,11 @@ function About() {
             <img className ="natepic" src={nate} alt="nate" />
             </div>
             <div className="paragraph">
-                <h2>Nate Hogan</h2>
+                <h2>Nathan Hogan</h2>
                 <p>
-                Lorem ipsum dolor sit amet. I bet you thought this who thing was filler text. To start 
-                off I like to have fun and enjoy myself in a professional environment.
-                I think it is very important to enjoy your job and when you do so you will want to work hard.
-                I am a very hard working and loyal person and that will show more once you get to know me. 
+                I am a hard working person who also likes fun working environments. I graduated from a coding bootcamp from the University of Utah to be a
+                full stack developer.  I am also continuing my education on my own time by studing by myself and some extra courses from my bootcamp.  I
+                promise to always give my best in any job that I do. If you are interested in learning more please reach out to me.
                 </p>
             </div>
         </section>
